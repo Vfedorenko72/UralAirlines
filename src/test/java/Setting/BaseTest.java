8ad6b719abf73e8686c1b29450893dfceebf4d5d
vitaliyDunice <v.stepanov@sh.dunice.net>
2024-10-28 10:06:17 +0300

    test4

--- src/test/java/Setting/BaseTest.java
+++ src/test/java/Setting/BaseTest.java
@@ -3,18 +3,19 @@ package Setting;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 //import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 
 abstract public class BaseTest {
     public static void setUp(){
-        //Автоматическое скачивание и настройка браузера хром
-       // WebDriverManager.chromedriver().setup();
+        //томатическое скачивание и настройка браузера хром
+        WebDriverManager.chromedriver().setup();
         //Выбор драйвера
-       // Configuration.browser = "chrome";
+        Configuration.browser = "chrome";
         //Разрешение экрана
-        Configuration.browserSize = "1000x800";
+        //Configuration.browserSize = "";
         //Отображение экрана
         Configuration.headless = false;
 
